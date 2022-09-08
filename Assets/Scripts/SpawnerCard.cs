96346d343745f994ae1f49bdea607a0eddefd4a8
Tilleul <thomas.picard@student.junia.com>
2022-09-08 16:23:13 +0200

    Spawn Card

--- Assets/Scripts/SpawnerCard.cs
+++ Assets/Scripts/SpawnerCard.cs
@@ -6,7 +6,10 @@ public class SpawnerCard : MonoBehaviour
 {
     private CardData cardDataAlea;
     private CardData whoSpawn;
-    private int CoordX;
+    private int coordX;
+    private float coordY =-3f;
+    private int coordZ = 0;
+    private float rotationZ = 0f;
     public GameObject card;
     public int nbCard;
     public List<CardData> cardPossible;
@@ -26,21 +29,35 @@ public class SpawnerCard : MonoBehaviour
 
     void SpawnCard()
     {
-        whoSpawn = WhatCardSpawn();
         for(int i =0; i<nbCard; i++)
         {
+            whoSpawn = WhatCardSpawn();
             if (i % 2 == 1 && i !=0)
             {
-                CoordX = (i / 2) + 1;
+                coordX = (i / 2) + 1;
+                rotationZ = ((i / 2) + 1)*(-10);
+                if(i == 1){
+                    coordY=-3.14f;
+                }
+                else if(i == 3){
+                    coordY=-3.46f;
+                }
+                else if(i == 5){
+                    coordY=-4f;
+                }
+                coordZ+=1;
             }
             if(i%2 == 0 && i != 0)
             {
-                CoordX = i / (-2);
+                coordX = i / (-2);
+                rotationZ = i / (-2)*(-10);
             }
-            GameObject cardPrefab = Instantiate(card, new Vector2(CoordX,0), Quaternion.identity);
+            GameObject cardPrefab = Instantiate(card, new Vector3(coordX,coordY,coordZ), Quaternion.identity);
+            cardPrefab.transform.Rotate(new Vector3(0, 0, rotationZ));
             cardPrefab.GetComponent<Card>().gA = cardDataAlea.gA;
             cardPrefab.GetComponent<Card>().gD = cardDataAlea.gD;
             cardPrefab.GetComponent<Card>().gP = cardDataAlea.gP;
+
         }
     }
 
